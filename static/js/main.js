8019fa52f5ad585c605b64b5012037ea4c0a72d5
Raza Haider <raxa.hai@gmail.com>
2020-06-12 21:08:21 +0500

    Initial Commit

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+console.log('working javascript');
+
+var typed = new Typed('#typing', {
+	strings:['Divine'],
+    typeSpeed: 80,
+    backSpeed: 50,
+    startDelay: 500,
+    showCursor: false,
+    loop: true,
+});
